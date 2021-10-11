3545f572a570318aac3e99396d9b60693286b00a
mabunde <solomonsitati39@gmail.com>
2021-10-11 20:09:16 +0300

    create submit button

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -8,12 +8,12 @@ import{Quotes} from'../quotes'
 })
 export class QuotesComponent implements OnInit {
   quotes:Quotes[] = [
-    new Quotes (1,'Charles','Human spirit','Albert Einstein','The human spirit must prevail over technology to allow humans be in charge.', new Date(2019,7,12),0,0),
-    new Quotes (2,'Wendy','Technology…','Max Frisch','Technology… the knack of so arranging  that we don’t have to experience it.',new Date(2019,7,14),0,0),
-    new Quotes (3,'Mikey','The great myth','Libby Larsen','The great myth of our times is that technology is communication.',new Date(2019,6,3),0,0),
-    new Quotes (4,'Beatrice','Communication','Clay Shirky','Communications tools don’t get socially interesting until they get technologically boring.',new Date(2019,7,7),0,0),
-    new Quotes (5,'Charles','Harold Abelson','Code and people','Programs must be written for people to read, and only incidentally for machines to execute.',new Date(2019,7,17),0,0),
-    new Quotes (6,'Alfred','Alice Kahn','Tech and life','For a list of all the ways technology has failed to improve the quality of life, please press three.',new Date(2019,7,15),0,0)
+    new Quotes (1,'Charles','Albert Einstein','"The human spirit must prevail over technology to allow humans be in charge."', new Date(2019,7,12),0,0),
+    new Quotes (2,'Wendy','Max Frisch','"Technology… the knack of so arranging  that we don’t have to experience it."',new Date(2019,7,14),0,0),
+    new Quotes (3,'Mikey','Libby Larsen','"The great myth of our times is that technology is communication."',new Date(2019,6,3),0,0),
+    new Quotes (4,'Beatrice','Clay Shirky','"Communications tools don’t get socially interesting until they get technologically boring."',new Date(2019,7,7),0,0),
+    new Quotes (5,'Charles','Code and people','"Programs must be written for people to read, and only incidentally for machines to execute."',new Date(2019,7,17),0,0),
+    new Quotes (6,'Alfred','Tech and life','"For a list of all the ways technology has failed to improve the quality of life, please press three."',new Date(2019,7,15),0,0)
   ];
 
   get sortQuotes(){
